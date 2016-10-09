67b574dc287adcbfc3439c2cdf7641b91f010f2a
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-09 09:28:17 -0400

    Initial commit

---
+++ RecipesApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RecipesEntities context = new RecipesEntities())
+            {
+
+                Recipe_Table recipe = new Recipe_Table
+                {
+                    Name = "Chicken Salad"
+                };
+
+                context.Recipe_Tables.Add(recipe);
+                context.SaveChanges(); //Always save changes and refresh
+               
+                
+                //Add a new recipe to our recipe table
+                //Like adding new object to our recipes collection
+                //context.Recipe_Tables
+            }
+               
+        }
+    }
+}
