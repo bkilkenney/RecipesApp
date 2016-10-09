6bb0e58361ec0d50754b13c93501d08d51d8e559
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-09 09:47:41 -0400

    Created db first model, added table, column & entry into db

--- RecipesApp/Program.cs
+++ RecipesApp/Program.cs
@@ -13,18 +13,32 @@ namespace RecipesApp
             using (RecipesEntities context = new RecipesEntities())
             {
 
-                Recipe_Table recipe = new Recipe_Table
-                {
-                    Name = "Chicken Salad"
-                };
+                Recipe_Table recipe = context.Recipe_Tables.FirstOrDefault(r => r.Name == "Chicken Salad");
+                //Above line is for searching chicken salad recipe, below line displays
+                //it's ID which we know is 1 b/c it's the only recipe in the table
+                Console.WriteLine(recipe.Id);
 
-                context.Recipe_Tables.Add(recipe);
-                context.SaveChanges(); //Always save changes and refresh
+
+
+//***********Above is how to select something from your table****
+
+//************Below was how to insert into your table****************
+                //Recipe_Table recipe = new Recipe_Table
+                //{
+                //    Name = "Chicken Salad"
+                //};
+
+                //context.Recipe_Tables.Add(recipe);
+                //context.SaveChanges(); //Always save changes and refresh
                
                 
                 //Add a new recipe to our recipe table
                 //Like adding new object to our recipes collection
                 //context.Recipe_Tables
+
+            //***********************************************************
+
+
             }
                
         }
